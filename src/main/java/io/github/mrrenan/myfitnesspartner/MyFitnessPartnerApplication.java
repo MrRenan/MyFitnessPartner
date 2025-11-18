011519b291237612548353776f1eca7b6723dc7a
Renan <renan.carli@hotmail.com>
2025-11-18 17:26:04 -0300

    Configurar projeto Spring Boot (Java 21+, Gradle)

---
+++ src/main/java/io/github/mrrenan/myfitnesspartner/MyFitnessPartnerApplication.java
@@ -0,0 +1,13 @@
+package io.github.mrrenan.myfitnesspartner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFitnessPartnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFitnessPartnerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/mrrenan/myfitnesspartner/MyFitnessPartnerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.mrrenan.myfitnesspartner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFitnessPartnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
